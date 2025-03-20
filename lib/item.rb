26c5556bfdbd33c8a684b5506125ba12225fb967
Catalin Leonte <catalin@autouncle.com>
2025-03-20 09:11:39 +0200

    Add Item logic

--- lib/item.rb
+++ lib/item.rb
@@ -1,2 +1,11 @@
 class Item
+  PRODUCT = Struct.new(:product_code, :name, :price)
+
+  PRODUCT_CODES = %w[GR1 SR1 CF1].freeze
+
+  PRODUCT_INVENTORY = {
+    "GR1" => PRODUCT.new("GR1", "Green Tea", 3.11),
+    "SR1" => PRODUCT.new("SR1", "Strawberries", 5.00),
+    "CF1" => PRODUCT.new("CF1", "Coffee", 11.23)
+  }.freeze
 end
